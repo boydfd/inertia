5cf5811fa76fc333b0e23b826f3969d6134b01c4
rlin <rlin@thoughtworks.com>
2017-05-15 17:15:01 +0800

    [@boydfd] add deploy_docker script.

---
+++ docker/deploy_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker rlin
+sudo systemctl enable docker
