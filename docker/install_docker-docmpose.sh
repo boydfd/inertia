957a3651ad0d38e932ce27ab7df5f460e6321e8c
rlin <rlin@thoughtworks.com>
2017-05-19 16:33:13 +0800

    [@boydfd] fix bug: redirect stderr to stdout, then check the stdout.

--- docker/install_docker-docmpose.sh
+++ docker/install_docker-docmpose.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-if $(docker-compose -v | grep 'command not found')
-then
-	installDockerCompose
-else
-	echo 'docker-composes already exist'
-fi
-
 installDockerCompose() {
 	curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 	path=/usr/local/bin
@@ -16,5 +9,12 @@ installDockerCompose() {
 	echo "docker-compose is installed to $path"
 	sudo chmod +x ${path}/docker-compose
 }
+
+if [ "$(docker-compose -v 2>&1 | grep 'command not found')" ]
+then
+	installDockerCompose
+else
+	echo 'docker-composes already exist'
+fi
 
 
