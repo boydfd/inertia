c3ba2425e3e5bfab4d01c85a8cf99c95c58e9da2
rlin <rlin@thoughtworks.com>
2017-05-19 16:43:46 +0800

    [@boydfd] delete useless sudo.

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum makecache fast
-sudo yum install -y docker-ce
-sudo systemctl start docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install -y docker-ce
+systemctl start docker
+groupadd docker
+usermod -aG docker $USER
+systemctl enable docker
