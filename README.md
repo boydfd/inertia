90b220b614a31da4926e69543ef08f162ab7604f
rlin <rlin@thoughtworks.com>
2017-07-07 09:51:59 +0800

    [@boydfd] fix bug: export user first before execute sudo command.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 1. 第一个脚本自然是自动安装docker：
   - 前提条件：拥有sudo执行权限
-  - 代码：`curl https://raw.githubusercontent.com/boydfd/inertia/master/docker/install_docker.sh | sudo bash`
+  - 代码：`export user=$USER;curl https://raw.githubusercontent.com/boydfd/inertia/master/docker/install_docker.sh | 
+  sudo -E bash`
 
 2. 自动安装docker-composes:
   - 前提：拥有sudo执行权限
--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+user=$1
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
 systemctl start docker
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker $user
 systemctl enable docker
 echo -e "\033[31myou have to reboot or logout and login to make it available to run docker without sudo.\033[0m"
